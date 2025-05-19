db6033891dfc36c57d6c74e3e4d6c2b89cc920ac
Ochygorek <ochygorek@email.cz>
2025-05-19 19:25:39 +0200

    Placeholder

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="w-full min-h-screen flex items-center justify-center font-[family-name:var(--font-space-mono)]">
+      <p>NullPoint</p>
+    </main>
+  );
+}
