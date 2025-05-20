0e65ceb1d12d3c71d909f0c1f1195aa5dd91e438
Ochygorek <ochygorek@email.cz>
2025-05-20 12:55:27 +0200

    Temporary/Placeholder

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,9 @@ export default function Home() {
   return (
     <main className="w-full min-h-screen flex items-center justify-center font-[family-name:var(--font-space-mono)] relative">
       <div className="relative flex items-center justify-center z-10 transition-all">
-        <p className="peer cursor-none">NullPoint</p>
-        <div className="duration-500 peer-hover:opacity-100 opacity-0 [clip-path:polygon(0%_0%,_38.25%_0%,_0%_38.25%,_0%_0%)] rotate-45 absolute -bottom-5 size-4 bg-white rounded-xs"></div>
-        <div className="duration-500 peer-hover:opacity-100 opacity-0 bg-white text-black text-xs rounded pb-1 pt-0.75 px-1.5 flex items-center justify-center w-fit absolute -bottom-7">
+        <p className="peer cursor-default">NullPoint</p>
+        <div className="duration-500 lg:peer-hover:opacity-100 lg:opacity-0 [clip-path:polygon(0%_0%,_38.25%_0%,_0%_38.25%,_0%_0%)] rotate-45 absolute -bottom-5 size-4 bg-white rounded-xs"></div>
+        <div className="duration-500 lg:peer-hover:opacity-100 lg:opacity-0 bg-white text-black text-xs rounded pb-1 pt-0.75 px-1.5 flex items-center justify-center w-fit absolute -bottom-7">
           <p>soon</p>
         </div>
       </div>
